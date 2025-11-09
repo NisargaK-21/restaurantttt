273180890cc20a2710c4a606291d18cfdceb8314
Nisarga <nisargakshetty@gmail.com>
2025-11-09 21:28:47 +0530

    goutami did about us page

--- app/about_us/page.js
+++ app/about_us/page.js
@@ -6,6 +6,7 @@ import React from "react";
 import Hero from "../components/hero";
 import RestanVedio from "../components/restan_vedio";
 import Chef from "../components/chef";
+import Button from "../components/button";
 
 export default function AboutUs() {
     return(
@@ -36,16 +37,35 @@ export default function AboutUs() {
                    </div>
                    
 
-                   <div className="flex flex-">
-                    <img src="https://www.daysoftheyear.com/wp-content/uploads/waiters-day.jpg"/>
-                    <img src=""/>
-                    <div>
-
+                   <div className="flex flex-col lg:flex-row gap-7 mt-20 relative bg-white p-5 lg:p-20">
+                    <img 
+                    className="w-[400px] h-[450px] object-cover z-0"
+                    src="https://www.daysoftheyear.com/wp-content/uploads/waiters-day.jpg"/>
+                    <img 
+                    className="w-[400px] h-[450px] object-cover z-0"
+                    src="https://www.appetite4recruitment.co.uk/wp-content/uploads/2023/10/Waitress-fine-dining_1-jpg.webp"/>
+                      
+                   <div>
+                     <img 
+                    className="w-[200px] h-[100px]  ml-90 translate-y-15 relative z-10"
+                    src="https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Fshape%2F2.png&w=3840&q=75"/>
+                    <div className=" relative bg-white sm:w-[200px] md:w-[600px] lg:w-[600px] sm:h-[400px]  md:h-[400px] lg:h-[400px] flex flex-col  p-10 sm:ml-2 lg:-ml-30 translate-y-10 shadow-xl rounded-md z-5">
+                        <h1 className=" text-2xl text-[#8b6b3b]">About us</h1>
+                        <br/>
+                        <h1 className="text-4xl text-black flex flex-wrap">We Invite You To Visit Our Restaurant</h1>
+                        <br/>
+                        <p className="text-gray-400 sm:w-[300px] md:w-[500px] lg:w-[500px]">A relaxing and pleasant atmosphere, good jazz, dinner, and cocktails. The Patio Time Bar opens in the center of Florence. The only bar inspired by the 1960s, it will give you a experience that you will have a hard time forgetting.</p>
+                        <br/>
+                        <Button
+                         className="bg-[#8b6b3b]"
+                         label="Discover"/>
                     </div>
                    </div>
+                   </div>
+                   
 
-
-                  <div className=" ">
+                  
+                  <div className=" mt-20 ">
                      <Chef/>
                   </div>
 
